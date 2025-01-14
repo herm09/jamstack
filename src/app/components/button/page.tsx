81b297124f33dbd50b6f8d52f4be823629b28cf9
Hermione Tetard <herm.tetard@gmail.com>
2025-01-14 13:42:06 +0100

    added export button

--- src/app/components/button/page.tsx
+++ src/app/components/button/page.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-type ButtonProps = {
+interface ButtonProps {
     label: string;
     onClick: () => void;
-};
+}
 
 const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
     return <button onClick={onClick}>{label}</button>;
-}
+};
 
 export default Button;
+export type { ButtonProps };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
+// import styles from "./page.module.css";
 import Button from "./components/button/page";
 
 export default function Home() {
